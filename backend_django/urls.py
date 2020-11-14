c32dd319dc2f03a831284889b5c4882acd94b1a0
juan sebastian monroy <juanse9514@hotmail.com>
2020-11-13 20:54:17 -0500

    db

--- backend_django/urls.py
+++ backend_django/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api_lerny/', include('lerny.urls')),
     path('api_user/', include('user.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
--- user/models.py
+++ user/models.py
@@ -5,28 +5,27 @@ from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager)
 
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, user_name, user_surname, country, city, passw, identification_number, mail):
+    def create_user(self, user_name, user_surname, country, city, mail,identification):
 
-        if not identification_number:
-            raise ValueError("debe ingresar numero de celular")
+        if not identification:
+            raise ValueError("debe ingresar numero de identificacion")
         user = self.model(
             user_name=user_name,
             mail=self.normalize_email(mail),
             user_surname=user_surname,
             country=country,
             city=city,
-            identification_number=identification_number)
-        user.set_password(identification_number)
+            identification=identification)
+        user.set_password(identification)
         user.save()
         return user
-    def create_superuser(self,user_name,user_surname,country,city,password,identification_number,mail):
+    def create_superuser(self,user_name,user_surname,country,city,password,mail,identification):
         user=self.create_user(user_name=user_name,
             user_surname=user_surname,
             country=country,
             city=city,
-            passw = identification_number,
-            identification_number=identification_number,
-            mail=mail)
+            mail=mail,
+            identification=identification)
         user.admin_user=True
         user.save()
         return user
@@ -37,8 +36,7 @@ class User(AbstractBaseUser):
     user_surname = models.CharField('user surname', max_length=50, null=False)
     country = models.CharField('country', max_length=20, null=False)
     city = models.CharField('city', max_length=20, null=False)
-    passw = models.CharField('passw', max_length=20, null=False)
-    identification_number = models.CharField('identification number',unique=True, max_length=20, null=False)
+    #passw = models.CharField('passw', max_length=20, null=False)
     mail = models.CharField('mail', max_length=100,unique=True,blank=True, null=True)
     notification = models.BooleanField(default=True)
     last_view_date = models.DateTimeField('last view date', null=True)
@@ -46,9 +44,10 @@ class User(AbstractBaseUser):
     creation_date = models.DateTimeField('creation date', auto_now_add=True)
     active_user = models.BooleanField(default = True)
     admin_user= models.BooleanField(default = False)
+    identification = models.CharField('identification',unique=True, max_length=20, default = "12345")
     objects=MyUserManager()
 
-    USERNAME_FIELD = "identification_number"
+    USERNAME_FIELD = "identification"
     REQUIRED_FIELDS =  ["user_name","user_surname","country","city","mail"]
 
     def __str__(self):

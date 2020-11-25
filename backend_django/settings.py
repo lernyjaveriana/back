4e7933f7639001b3840f13ba8742b3dddfcbee39
juan sebastian monroy <juanse9514@hotmail.com>
2020-11-24 23:46:30 -0500

    true

--- backend_django/settings.py
+++ backend_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+k5kxfbw2^arb(91&@u6sr_8st83rdcw)3jn1!#=9ndzh1v8!_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

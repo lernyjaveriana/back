028dd8deaf4f89bbad88a06ce7483f70254a02ff
ecantor12 <esteban.cantor@gmail.com>
2020-12-18 13:09:57 -0500

    update admin

--- lerny/admin.py
+++ lerny/admin.py
@@ -1,11 +1,78 @@
 from django.contrib import admin
-from .models import Lerny, MicroLerny, TreeMicroLerny, Resource, User_State
+from .models import *
 
 # Register your models here.
 
 
-admin.site.register(Lerny)
-admin.site.register(MicroLerny)
-admin.site.register(TreeMicroLerny)
-admin.site.register(Resource)
-admin.site.register(User_State)
+#admin.site.register(Lerny)
+@admin.register(Lerny)
+class LernyAdmin(admin.ModelAdmin):
+	list_display = ("pk", "lerny_name", "description", "url_image", "category", "price", "creation_date")
+
+@admin.register(MicroLerny)
+class MicroLernyAdmin(admin.ModelAdmin):
+	list_display = ("pk", "micro_lerny_title", "micro_lerny_subtitle", "update_date", "creation_date", "get_lerny")
+
+	def get_lerny(self, obj):
+		return obj.lerny.lerny_name
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+	list_display = ("pk", "title", "description", "content_url", "content_type", "phase", "creation_date", "points", "get_microlerny")
+
+	def get_microlerny(self, obj):
+		return obj.microlerny.micro_lerny_title
+
+@admin.register(TreeMicroLerny)
+class TreeMicroLernyAdmin(admin.ModelAdmin):
+	list_display = ("pk", "get_dady_microlerny", "get_son_microlerny")
+
+	def get_dady_microlerny(self, obj):
+		return obj.dady_micro_lerny.micro_lerny_title
+	
+	def get_son_microlerny(self, obj):
+		return obj.son_micro_lerny.micro_lerny_title
+
+
+@admin.register(User_State)
+class UserStateAdmin(admin.ModelAdmin):
+	list_display = ("pk", "get_lerny", "get_microlerny","get_user", "get_resource", "last_view_date")
+
+	def get_lerny(self, obj):
+		return obj.lerny_id.lerny_name
+	
+	def get_microlerny(self, obj):
+		return obj.micro_lerny_id.micro_lerny_title
+
+	def get_user(self, obj):
+		return obj.user_id.identification
+
+	def get_resource(self, obj):
+		return obj.resource_id.title
+
+
+@admin.register(User_Lerny)
+class UserLernyAdmin(admin.ModelAdmin):
+	list_display = ("pk", "get_lerny", "get_microlerny","get_user", "get_resource", "last_view_date")
+
+	def get_lerny(self, obj):
+		return obj.lerny_id.lerny_name
+	
+	def get_microlerny(self, obj):
+		return obj.micro_lerny_id.micro_lerny_title
+
+	def get_user(self, obj):
+		return obj.user_id.identification
+
+	def get_resource(self, obj):
+		return obj.resource_id.title
+
+@admin.register(User_Resource)
+class UserLernyAdmin(admin.ModelAdmin):
+	list_display = ("pk", "get_resource", "user_id","done", "user_response", "response_date", "last_view_date", "done_date")
+
+	def get_resource(self, obj):
+		return obj.resource_id.title
+	
+	def get_user(self, obj):
+		return obj.user_id.identification
--- user/admin.py
+++ user/admin.py
@@ -2,27 +2,26 @@ from django.contrib import admin
 from .models import User
 # Register your models here.
 
-admin.site.register(User)
+#admin.site.register(User)
 
-#@admin.register(User)
-#class UserAdmin(admin.ModelAdmin):
-#    """User admin."""
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """User admin."""
 
-#    list_display = ('pk', 'username', 'email',)
-#    list_display_links = ('pk', 'username', 'email',)
+    list_display = ('pk', 'identification', 'mail', 'user_name', 'active_user', 'admin_user')
+    #list_display_links = ('pk', 'user_name', 'mail',)
 
-#    search_fields = (
-#        'email',
-#        'username',
-#        'first_name',
-#        'last_name',
-#    )
+    search_fields = (
+        'mail',
+        'user_name',
+        'first_name',
+        'last_name',
+    )
 
-#    list_filter = (
-#        'is_active',
-#        'is_staff',
-#        'date_joined',
-#        'modified',
-#    )
+    list_filter = (
+        'active_user',
+        'admin_user',
+        'identification',
+    )
 
-#    readonly_fields = ('date_joined', 'modified',)
+    #readonly_fields = ('date_joined', 'modified',)

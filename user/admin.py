1fd1fff5f8e53cb56bb324034ea882b14b1847f4
juan sebastian monroy <juanse9514@hotmail.com>
2021-04-06 11:17:57 -0500

    cambios

--- user/admin.py
+++ user/admin.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from .models import User
 # Register your models here.
-
-admin.site.register(User)
-@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("user_name", "user_surname")
 
-
+admin.site.register(User, UserAdmin)
 #@admin.register(User)
 #class UserAdmin(admin.ModelAdmin):
 #    """User admin."""

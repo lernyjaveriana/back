2517cdb05b68fdb06945bcc3a65d20839049d931
juan sebastian monroy <juanse9514@hotmail.com>
2021-04-06 11:26:43 -0500

    cambios

--- lerny/admin.py
+++ lerny/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from .models import Lerny, MicroLerny, TreeMicroLerny, Resource, User_State, User_Resource
 
 # Register your models here.
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("lerny_name")
 
-admin.site.register(Lerny,UserAdmin)
+
+admin.site.register(Lerny)
 admin.site.register(MicroLerny)
 admin.site.register(TreeMicroLerny)
 admin.site.register(Resource)

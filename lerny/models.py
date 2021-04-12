ad0d8c676f8404afdca54ece425931709063f528
juan sebastian monroy <juanse9514@hotmail.com>
2021-04-12 09:32:39 -0500

    cvambios

--- lerny/models.py
+++ lerny/models.py
@@ -31,11 +31,12 @@ class Resource(models.Model):
 	description = models.CharField('description', max_length = 300)
 	content_url = models.CharField('content_url', max_length = 100, null=False)
 	content_type = models.CharField('microlerny title', max_length = 200, null=False)
+	image_url = models.CharField('image url', max_length = 200, null=False)
 	phase = models.CharField('phase', max_length = 3, null=False)
 	creation_date = models.DateTimeField('creation date', auto_now_add = True)
 	points = models.FloatField('points', null=False)
 	microlerny = models.ForeignKey(MicroLerny, on_delete=models.CASCADE, null = False)
-	image_url = models.CharField('image url', max_length = 200, null=False)
+	
 	def __str__(self):
 		return f'{self.title,self.phase}'
 class User_Lerny(models.Model):

7522af3e4b5e923e9a6386d722cdde369cd74c67
juan sebastian monroy <juanse9514@hotmail.com>
2021-05-19 21:14:46 -0500

    Login perpetuo

--- user/views.py
+++ user/views.py
@@ -698,7 +698,123 @@ class ApiManager(APIView):
 						},
 					]
 				}
-			
+		# BIENVENIDO_LERNY
+		elif(key == "BIENVENIDO_LERNY"):
+			if(user_id):
+				user = User.objects.get(
+				identification=user_id)
+				data = {
+					"fulfillmentMessages": [
+						{
+							"text": {
+								"text": [
+									"Bienvenido a lerny"
+								]
+							}
+						},
+						{
+							"payload": {
+								"facebook": {
+									"attachment": {
+										"type": "template",
+										"payload":
+												{
+													"template_type": "generic",
+													"elements":
+													[
+														{
+															"title": "Hola " + UserSerializer(user).data["user_name"] + ", un gusto volver a verte!",
+															"image_url": "https://lerny.co/wp-content/uploads/2020/12/titulo_curso.jpg",
+															"subtitle": "Para comenzar por favor selecciona una opción.",
+															"buttons":
+															[
+																{
+																	"type": "postback",
+																	"title": "Continuar Lerny",
+																	"payload": "CONTINUAR_CURSO"
+																},
+																{
+																	"type": "postback",
+																	"title": "ver Micro Lernys",
+																	"payload": "LIST_MICROLERNYS"
+																},
+																{
+																	"type": "postback",
+																	"title": "Listar Lernys",
+																	"payload": "LISTAR_LERNYS"
+																}
+															]
+														}
+													]
+												}
+									}
+								}
+							}
+						}
+					]
+				}
+			else:
+				data = {
+					"fulfillmentMessages": [
+						{
+							"text": {
+								"text": [
+									"Hola!, te damos la bienvenida a Lerny, una plataforma de educación en facebook messenger, pensada para quienes construimos futuro trabajando."
+								]
+							}
+						},
+						{
+							"payload": {
+								"facebook": {
+									"attachment": {
+										"type": media,
+										"payload": {
+											"attachment_id":data["content_url"]
+										}
+									}
+								}
+							}
+						},
+						{
+							"payload":{
+								"facebook": 
+								{
+									"attachment": 
+									{
+									"type": "template",
+									"payload": {
+										"template_type": "generic",
+										"elements": [
+										{
+											"image_url": "https://lerny.co/wp-content/uploads/2020/12/marca_lerny.jpg",
+											"buttons": [
+											{
+												"title": "Comprar curso",
+												"payload": "comprar_curso",
+												"type": "postback"
+											},
+											{
+												"payload": "iniciar_sesion",
+												"title": "Iniciar sesión",
+												"type": "postback"
+											},
+											{
+												"payload": "info_contacto",
+												"title": "Más información",
+												"type": "postback"
+											}
+											],
+											"title": "Menú Principal",
+											"subtitle": "Para comenzar por favor selecciona una opción."
+										}
+										]
+									}
+									}
+								}
+								}
+						}
+					]
+				}
 		else:
 			data = {}
 		print(data)

e4c7fd5a3445f8b0bc2d66053248f3ee20cdc58f
juan sebastian monroy <juanse9514@hotmail.com>
2021-05-27 12:48:02 -0500

    print2

--- user/views.py
+++ user/views.py
@@ -140,7 +140,7 @@ class ApiManager(APIView):
 			# lerny = request['LERNY_INTENT']
 			serializers_class = MicroLernySerializer
 			lerny_active = User_Lerny.objects.filter(active=True).first()
-			print("üserlerny serializer: "+UserLernySerializer(lerny_active))
+			print("userlerny serializer: "+UserLernySerializer(lerny_active).data)
 			micro_lerny = MicroLerny.objects.filter(lerny=lerny_active)
 			data = MicroLernySerializer(micro_lerny, many=True).data
 			i = 0

f78ab6b9f1b4942ce655939b7f5616665b20631a
juan sebastian monroy <juanse9514@hotmail.com>
2021-06-10 21:56:53 -0500

    name of resource type

--- lerny/models.py
+++ lerny/models.py
@@ -39,7 +39,7 @@ class Resource(models.Model):
 	microlerny = models.ForeignKey(MicroLerny, on_delete=models.CASCADE, null = False)
 	image_url = models.CharField('image url', max_length = 200, null=False)
 	media_type = models.CharField('media type', max_length = 200, null=False)
-	resource_type = models.CharField('media type', max_length = 200, null=False)
+	resource_type = models.CharField('resource type', max_length = 200, null=False)
 	def __str__(self):
 		return f'{self.title,self.phase}'
 class User_Lerny(models.Model):

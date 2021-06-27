bc9f83dff8830a9e1fe04e934c02a7695c6f1bcb
ecantor12 <esteban.cantor@gmail.com>
2021-06-27 08:56:18 -0500

    datatable and login

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
 # Register your models here.
+"""
 class UserAdmin(admin.ModelAdmin):
     list_display = ("user_name", "user_surname", "identification", "company", "group")
     search_fields =("identification",)
 admin.site.register(User, UserAdmin)
+"""
 #@admin.register(User)
 #class UserAdmin(admin.ModelAdmin):
 #    """User admin."""
@@ -27,3 +32,25 @@ admin.site.register(User, UserAdmin)
 #    )
 
 #    readonly_fields = ('date_joined', 'modified',)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('identification',)
+    list_filter = ('identification',)
+    fieldsets = (
+        (None, {'fields': ('user_name', 'user_surname', 'country', 'city', 'uid', 'mail', 'notification', 'last_view_date', 'points', 'active_user', 'admin_user', 'identification', 'company', 'group', 'password')}),
+        ('Permissions', {'fields': ()}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('user_name', 'user_surname', 'country', 'city', 'uid', 'mail', 'notification', 'last_view_date', 'points', 'active_user', 'admin_user', 'identification', 'company', 'group', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('identification',)
+    ordering = ('identification',)
+
+admin.site.register(User, CustomUserAdmin)
+

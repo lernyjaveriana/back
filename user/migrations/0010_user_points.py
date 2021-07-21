1253f6601da55e9a8fbdccb98aa83dc71470793c
juan sebastian monroy <juanse9514@hotmail.com>
2021-07-21 07:16:43 -0500

    add field points delete

--- user/migrations/0010_user_points.py
+++ user/migrations/0010_user_points.py
@@ -10,9 +10,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='user',
-            name='points',
-            field=models.FloatField(blank=True, null=True, verbose_name='points'),
-        ),
     ]

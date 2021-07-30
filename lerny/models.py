8395598b0bd5f7b051a7422b59f365cc18291daf
juan sebastian monroy <juanse9514@hotmail.com>
2021-07-29 21:11:11 -0500

    media

--- lerny/models.py
+++ lerny/models.py
@@ -49,7 +49,12 @@ class Resource(models.Model):
 	def __str__(self):
 		return f'{self.title,self.phase,self.microlerny}'
 
-
+class Media(models.Model):
+	resource_id = models.ForeignKey('resource',on_delete=models.CASCADE, null = False)
+	content_url = models.CharField('content_url', max_length = 100, null=False)
+	content_type = models.CharField('content type', max_length = 200, null=False)
+	def __str__(self):
+		return f'{self.resource_id,self.content_type}'
 class User_Lerny(models.Model):
 	lerny_id = models.ForeignKey(Lerny, on_delete=models.CASCADE, null = False)
 	user_id = models.ForeignKey(User, on_delete=models.CASCADE, null = False)

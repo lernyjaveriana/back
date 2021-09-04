ac3d74a14728508fc8cc88aac9aaf537b02dc75e
juan sebastian monroy <juanse9514@hotmail.com>
2021-09-03 21:46:57 -0500

    restruct

--- user/views.py
+++ user/views.py
@@ -8,10 +8,53 @@ from .models import User
 from lerny.models import *
 from lerny.serializers import *
 from datetime import datetime
-import s3upload
-import constants
+
+import os
+import boto3
+import wget
+from botocore.exceptions import ClientError
 
 
+id_key = "AKIA4K6QA2LE547NHTXD"
+access_secret= "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"
+bucket_name = "lerny-responses"
+region='us-east-2'
+folder=''
+
+def build_fileurl(bucket_name,region,folder,filename):
+    url = f"https://{bucket_name}.s3.{region}.amazonaws.com/{folder}{filename}"
+    return url
+
+def upload_to_s3(from_url,id_key,access_secret,bucket_name,region,folder):
+
+    filename = wget.download(from_url)
+    print(filename)
+
+    client_s3 =  boto3.client(
+        's3',
+        aws_access_key_id = id_key,
+        aws_secret_access_key = access_secret 
+    )
+
+    data_file_folder= os.getcwd()
+
+    try:
+        print('uploading '+ filename +' to bucket')
+        client_s3.upload_file(
+            os.path.join(data_file_folder,filename),
+            bucket_name,
+            filename
+        )
+    except ClientError as e:
+        print('credentials incorrect')
+        print(e)
+        return e
+    except Exception as e:
+        print(e)
+        return e
+    
+    return build_fileurl(bucket_name,region,folder,filename)
+    
 class UserManageGet(APIView):
 
 	serializers_class = UserSerializer
@@ -676,7 +719,7 @@ class ApiManager(APIView):
 				u_resource = User_Resource.objects.filter(user_id=user_id_obj, resource_id=user_state.resource_id).first()
 				
 				if (response.split(':')[0] == 'https'):
-					response=s3upload.upload_to_s3(response,constants.id_key,constants.access_secret,constants.bucket_name,constants.region,constants.folder)
+					response=upload_to_s3(response,id_key,access_secret,bucket_name,region,folder)
 				
 				if(u_resource):
 					data = UserResourceSerializer(u_resource).data

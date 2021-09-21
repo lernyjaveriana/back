a0fc5b6ae388e7db05c5553b4e66c4daef71ad17
juan sebastian monroy <juanse9514@hotmail.com>
2021-09-21 12:34:41 -0500

    previous text bigger capacity

--- lerny/models.py
+++ lerny/models.py
@@ -32,7 +32,7 @@ class Resource(models.Model):
 	description = models.CharField('description', max_length = 300)
 	content_url = models.CharField('content_url', max_length = 100, null=False)
 	content_type = models.CharField('microlerny title', max_length = 200, null=False)
-	previous_text = models.CharField('previous text', max_length = 200, blank=True)
+	previous_text = models.CharField('previous text', max_length = 600, blank=True)
 	phase = models.CharField('phase', max_length = 3, null=False)
 	creation_date = models.DateTimeField('creation date', auto_now_add = True)
 	points = models.FloatField('points', default=1, blank=True)

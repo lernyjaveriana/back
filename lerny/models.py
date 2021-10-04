33b2760f5bebfdd857591a13bc57f2cfc70f2a9f
fetex <dicson@javerianacali.edu.co>
2021-10-03 20:57:15 -0500

    space between class

--- lerny/models.py
+++ lerny/models.py
@@ -13,6 +13,7 @@ class Lerny(models.Model):
 
 	def __str__(self):
 		return f'{self.lerny_name}'
+
 class MicroLerny(models.Model):
 	micro_lerny_title = models.CharField('microlerny title', max_length = 200, null=False)
 	micro_lerny_subtitle = models.CharField('microlerny subtitle', max_length = 300, null=False)
@@ -22,11 +23,13 @@ class MicroLerny(models.Model):
 	lerny = models.ForeignKey(Lerny, on_delete=models.CASCADE, null = False)
 	def __str__(self):
 		return f'{self.micro_lerny_subtitle},{self.micro_lerny_title}'
+
 class TreeMicroLerny(models.Model):
 	dady_micro_lerny = models.ForeignKey(MicroLerny, on_delete=models.CASCADE, related_name='dady_micro_lerny')
 	son_micro_lerny = models.ForeignKey(MicroLerny, on_delete=models.CASCADE, related_name='son_micro_lerny')
 	def __str__(self):
 		return f'{self.dady_micro_lerny},{self.son_micro_lerny}'
+
 class Resource(models.Model):
 	title = models.CharField('title', max_length = 100)
 	description = models.CharField('description', max_length = 300)
@@ -56,6 +59,7 @@ class Media(models.Model):
 	position = models.FloatField('position', null=False)
 	def __str__(self):
 		return f'{self.resource_id,self.content_type}'
+
 class User_Lerny(models.Model):
 	lerny_id = models.ForeignKey(Lerny, on_delete=models.CASCADE, null = False)
 	user_id = models.ForeignKey(User, on_delete=models.CASCADE, null = False)
@@ -71,6 +75,7 @@ class User_Lerny(models.Model):
 
 	def __str__(self):
 		return f'{self.user_id},{self.lerny_id}'
+
 class User_Resource(models.Model):
 	resource_id = models.ForeignKey(Resource, on_delete=models.CASCADE,null = False)
 	user_id = models.ForeignKey(User, on_delete=models.CASCADE, null = False)
@@ -82,6 +87,7 @@ class User_Resource(models.Model):
 	points = models.FloatField('points', null=True)
 	def __str__(self):
 		return f'{self.resource_id},{self.user_id}'
+
 class User_Micro_Lerny(models.Model):
 	micro_lerny_id = models.ForeignKey(MicroLerny, on_delete=models.CASCADE, null = False)
 	user_id = models.ForeignKey(User, on_delete=models.CASCADE, null = False)
@@ -89,6 +95,7 @@ class User_Micro_Lerny(models.Model):
 	last_view_date = models.DateTimeField('last view date', null=True)
 	def __str__(self):
 		return f'{self.user_id},{self.micro_lerny_id}'
+
 class User_State(models.Model):
 	lerny_id = models.ForeignKey(Lerny, on_delete=models.CASCADE, null = False)
 	micro_lerny_id = models.ForeignKey(MicroLerny, on_delete=models.CASCADE, null = False)

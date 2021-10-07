60672b82f0697a9d7288d76d97335682945ea489
Dicson <dicson@javerianacali.edu.co>
2021-10-07 00:59:38 -0500

    change return charts

--- lerny/views.py
+++ lerny/views.py
@@ -116,7 +116,7 @@ def charts(request):
 	except:
 		company = None
 	context = {"username": user.user_name, 'have_company': True if company != None else False}
-	return render(request, 'templates/base.html', context)
+	return render(request, '/lerny/templates/base.html', context)
 
 @csrf_exempt
 def editStateResource(request):

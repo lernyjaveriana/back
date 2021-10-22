376c7e1b74cef2fd4ea58043230dc44254ef885d
juan sebastian monroy <juanse9514@hotmail.com>
2021-10-21 23:15:39 -0500

    attempt carousel for practical resources

--- user/views.py
+++ user/views.py
@@ -124,6 +124,60 @@ cargarActividad={
 						}
 					]
 				}
+# After upload activities trggered into fallback intent, it should shows you a menu
+def cargarActividadFallbackIntent(user_id):
+	user_id_obj = User.objects.get(identification=user_id)
+	user_lerny = User_Lerny.objects.filter(active=True,user_id=user_id_obj).first()
+	lerny_active=user_lerny.lerny_id
+	micro_lernys = MicroLerny.objects.filter(lerny=lerny_active).order_by('pk')
+	previous_text = "A cual recurso deseas asociar el entregable que acabas de subir?"
+	for micro_lerny in micro_lernys:
+		resources_microlerny = Resource.objects.filter(microlerny=micro_lerny,resource_type="practical")
+		data = ResourceSerializer(resources_microlerny, many=True).data
+		i = 0
+		temp = []
+		
+		while(i < len(data)):
+			print("IMPRESION LISTAR RECURSOS PRACTICOS: "+ str(data[i]['id'])+") " + data[i]['title'])
+			temp.append(
+				{
+					"subtitle": data[i]['description'],
+					"image_url": data[i]['image_url'],
+					"title": data[i]['title'],
+					"buttons": [
+					{
+						"payload": "cargar actividad a este recurso"+str(data[i]['id']),
+						"title": "Cargar",
+						"type": "postback"
+					}
+					]
+				},)
+			i += 1
+
+	data = {
+		"fulfillmentMessages": [
+		{
+			"text": {
+				"text": [
+					previous_text
+				]
+			}
+		},
+		{
+			"payload": {
+				"facebook": {
+					"attachment": {
+						"type": "template",
+						"payload": {
+							"template_type": "generic",
+							"elements": temp
+						}
+					}
+				}
+			}
+		}]
+	}
+
 def mediaResponseFormat(resourse):
 	medias = Media.objects.filter(resource_id=resourse).order_by('position')
 	template=[]
@@ -968,7 +1022,7 @@ class ApiManager(APIView):
 								u_resource.response_date = datetime.now()
 								u_resource.last_view_date = datetime.now()
 								u_resource.save()
-						data = cargarActividad
+						data = cargarActividadFallbackIntent(user_id)
 		# CARGAR_REQ_MICROLERNY
 		elif(key == "PREGUNTA_GENERAL"):
 			question = request['QUESTION']

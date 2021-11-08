b20e986433efd70f0a8e341ec3ac1d6dc20700bd
juan sebastian monroy <juanse9514@hotmail.com>
2021-11-07 22:54:34 -0500

    files

--- user/views.py
+++ user/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import action
 from .models import User
 from lerny.models import *
 from lerny.serializers import *
+from user.bucketHelper import upload_to_s3
 from datetime import datetime
 
 import os
@@ -21,42 +22,7 @@ bucket_name = "lerny-responses"
 region='us-east-2'
 folder=''
 
-def build_fileurl(bucket_name,region,folder,filename):
-    url = f"https://{bucket_name}.s3.{region}.amazonaws.com/{folder}{filename}"
-    return url
 
-def upload_to_s3(from_url,id_key,access_secret,bucket_name,region,folder):
-
-    filename = wget.download(from_url)
-    print(filename)
-
-    os.rename(filename,filename.replace(" ",""))
-
-    client_s3 =  boto3.client(
-        's3',
-        aws_access_key_id = id_key,
-        aws_secret_access_key = access_secret 
-    )
-
-    data_file_folder= os.getcwd()
-
-    try:
-        print('uploading '+ filename +' to bucket')
-        client_s3.upload_file(
-            os.path.join(data_file_folder,filename),
-            bucket_name,
-            filename
-        )
-    except ClientError as e:
-        print('credentials incorrect')
-        print(e)
-        return e
-    except Exception as e:
-        print(e)
-        return e
-    
-    return build_fileurl(bucket_name,region,folder,filename)
-    
 class UserManageGet(APIView):
 
 	serializers_class = UserSerializer

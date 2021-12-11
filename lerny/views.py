1165dc633cb6e5ab55ab455983d6bd231d0b89fa
juan sebastian monroy <juanse9514@hotmail.com>
2021-12-11 16:22:13 -0500

    fix 2

--- lerny/views.py
+++ lerny/views.py
@@ -208,7 +208,7 @@ class lernyDetail(APIView):
 				for i in user_lerny:
 					data = {}
 					try:
-						group_id=UserGroupSerializer(User_Group.objects.get(User_id=i.user_id, group_id__lerny_id__pk=lerny.pk)).data["Group_id"] #grupo del usuario
+						group_id=UserGroupSerializer(User_Group.objects.get(User_id=i.user_id, Group_id__lerny_id__pk=lerny.pk)).data["Group_id"] #grupo del usuario
 						data['Grupo'] = GroupSerializer(Group.objects.get(pk=group_id,lerny_id=i.lerny_id)).data["Group_name"]
 					except AssertionError:
 						print("Error: "+AssertionError)

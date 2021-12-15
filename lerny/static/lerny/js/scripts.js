fe94be7909a5fe7ea64ee9822a0a6bc5307c5dca
Dicson <dicson@javerianacali.edu.co>
2021-12-15 01:29:33 -0500

    test buttons excel

---
+++ lerny/static/lerny/js/scripts.js
@@ -0,0 +1,21 @@
+
+
+
+
+$(document).ready(function () {
+    $('#datatables').DataTable({
+
+        dom: 'Bfrtip',
+        buttons: [
+            'excelHtml5','copy', 'csv', 'excel', 'pdf'
+        ],
+    });
+});
+
+
+
+
+
+
+
+    

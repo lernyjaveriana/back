400327912342eca6bfd62346fb1ae84a0d386c93
juan sebastian monroy <juanse9514@hotmail.com>
2022-03-16 17:09:17 -0500

    fix list users

--- lerny/views.py
+++ lerny/views.py
@@ -86,7 +86,7 @@ def ApiStateResource(request):
 		if company:
 			#filtro todos los lernys que pertenecen a la empresa que se encuentra asignado el colaborador
 			lernys = Lerny.objects.filter(lerny_company__company_id=company.pk).values_list("pk", flat=True)
-			user_resources = User_Resource.objects.filter(resource_id__microlerny__lerny__in=lernys).order_by("resource_id__microlerny__lerny__lerny_name") #user_resource son los entregables 
+			user_resources = User_Resource.objects.filter(resource_id__microlerny__lerny__in=lernys).filter(user_id__company__pk = company.pk).order_by("resource_id__microlerny__lerny__lerny_name") #user_resource son los entregables 
 			for i in user_resources:
 				data = {}
 				try:

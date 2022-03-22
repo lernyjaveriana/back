2c4be8cd2d14b048c7a4461833f016d121045f05
juan sebastian monroy <juanse9514@hotmail.com>
2022-03-22 08:23:23 -0500

    deploy fix

--- user/Intents/cargarActividadFallbackIntent.py
+++ user/Intents/cargarActividadFallbackIntent.py
@@ -5,6 +5,7 @@ from TemplateUtilities.interfaceTemplates import *
 
 # After upload activities trggered into fallback intent, it should shows you a menu
 def cargarActividadFallbackIntent(user_id):
+	
 	user_id_obj = User.objects.get(identification=user_id)
 	user_lerny = User_Lerny.objects.filter(active=True,user_id=user_id_obj).first()
 	lerny_active=user_lerny.lerny_id
